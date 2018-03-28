c3a10ccbf4106002f8ab09a5a1b060fbdaf46304
Ryan Hu <ryanhuuu@gmail.com>
2018-03-28 21:41:35 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,7 +3,7 @@ def nyc_pigeon_organizer(data)
   data.each do |t1key, t1val|
     if t1key == :color || t1key == :gender
       t1val.each do |t2key, t2val|
-        
+        result[t2key] = 
       end
     end
   end
